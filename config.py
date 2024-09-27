7fc91ff425928d2be6d835035ce1fb7e84e85eb1
Kadu Oliveira <carloseduardo_al@hotmail.com>
2024-09-27 06:43:27 -0300

    atualizar no banco

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+SECRET_KEY = 'alura'
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = 'sumimasen',
+        servidor = 'localhost',
+        database = 'jogoteca'
+    )
---
+++ views.py
@@ -0,0 +1,85 @@
+from flask import render_template, request, redirect, session, flash, url_for
+from jogoteca import app, db
+from models import Jogos, Usuarios
+
+titulo = 'Jogos Maneiros Demais'
+
+@app.route('/')
+def index():
+    lista = Jogos.query.order_by(Jogos.id)
+    print('Abrindo a página inicial.')
+    return render_template('lista.html', titulo=titulo, jogos = lista)
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        print('Redirecionado pro login.')
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo = 'Novo Jogo')
+
+@app.route('/criar', methods = ['POST' ,])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogos.query.filter_by(nome=nome).first()
+
+    # testando se o jogo já existe
+    if jogo:
+        flash('Esse jogo já existe no cadastro')
+        return redirect(url_for('index'))
+    # inserindo no banco de dados
+    novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+    print(f'O novo jogo "{nome}" foi criado e enviado para o banco de dados.')
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        print('Redirecionado para a página de edição de Jogos.')
+        return redirect(url_for('login', proxima=url_for('editar')))
+    jogo = Jogos.query.filter_by(id=id).first()
+    return render_template('editar.html', titulo = 'Editando Jogos', jogo=jogo)
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    # Localizando o objeto no banco de dados
+    jogo = Jogos.query.filter_by(id=request.form['id']).first()
+    # Alocando novos valores buscando no formulario
+    jogo.nome = request.form['nome']
+    jogo.categoria = request.form['categoria']
+    jogo.console = request.form['console']
+    # Gravando as informações no banco
+    db.session.add(jogo)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST' ,])
+def autenticar():
+    usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
+    if usuario:
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname  + ' foi logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            print(f"Redirecionando para: {proxima_pagina}")  # Mensagem de depuração
+            return redirect(proxima_pagina)
+    else:
+        flash('Usuário não logado.')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
